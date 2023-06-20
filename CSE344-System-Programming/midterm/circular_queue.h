bc5fa5fcba0309a62f39e743fe2cf3dcdb1c668d
muhammedsinan18 <pehlivanoglumuhammedsinan@gmail.com>
2023-06-20 10:35:52 +0300

    midterm

---
+++ CSE344-System-Programming/midterm/circular_queue.h
@@ -0,0 +1,20 @@
+#ifndef __CIRCULAR_QUEUE_H
+#define __CIRCULAR_QUEUE_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+
+#endif
+
+typedef struct{
+	int rear;
+	int front;
+	unsigned size;
+	long int *array;
+}Queue;
+
+Queue *queueInitialize(unsigned);
+void enqueue(Queue * queue, int item);
+long int dequeue(Queue * queue);
+void display(Queue *);
